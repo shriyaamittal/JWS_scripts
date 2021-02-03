baa01815c9f9a02839bc34e009c3051247cb4458
shriyaamittal <mittalshriyaa@gmail.com>
2021-02-03 18:17:45 -0500

    parseFEP_analysis.py

--- parseFEP_analysis.py
+++ parseFEP_analysis.py
@@ -46,7 +46,7 @@ data_f.close()
 data=np.array(data)
 data_backward=np.transpose(data)
 
-plt.errorbar(data_forward[0],data_forward[2],yerr=data_forward[3],color='k',"forward")
+plt.errorbar(data_forward[0],data_forward[2],yerr=data_forward[3],color='k',label="forward")
 plt.errorbar(data_backward[0],data_backward[2],yerr=data_backward[3],color='grey',label="backward")
 
 plt.ylabel(r"$\Delta$G")
